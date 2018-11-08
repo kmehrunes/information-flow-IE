dd3d263e2830ee179c890f598a4cfbd06d16151b
KLD <kld_at7@hotmail.com>
2018-11-08 19:37:34 +0300

    completed the rest of the documentation

--- src/main/scala/ie/PathExtraction.scala
+++ src/main/scala/ie/PathExtraction.scala
@@ -6,12 +6,32 @@ import util.CoreNLPUtil
 
 import scala.collection.mutable.ListBuffer
 
+/**
+  * The basic operations to extract information
+  * paths and their parts.
+  */
 object PathExtraction {
+  /**
+    * Creates a predicate object out of the predicate
+    * word. Finds the rest of words which make a
+    * complete predicate and its auxiliary branches.
+    * @param graph
+    * @param word
+    * @return
+    */
   def predicateFromWord(graph: SemanticGraph, word: IndexedWord): Predicate = {
     Predicate(word, DependencyGraphs.findUp(graph, word, Patterns.PREDICATE_PARTS /*COMPOUND_ONLY*/),
       BranchExtraction.findAuxiliaryBranches(graph, word, Patterns.PREDICATE_AUX))
   }
 
+  /**
+    * Creates a list of words which make the full
+    * object out of an object word. Finds compounds
+    * and modifiers.
+    * @param graph
+    * @param objectEdge
+    * @return
+    */
   def getObjectWords(graph: SemanticGraph, objectEdge: SemanticGraphEdge): List[IndexedWord] = {
     // this is only the last word in the object words
     val objectWord = objectEdge.getDependent
@@ -22,6 +42,13 @@ object PathExtraction {
     obj ::: DependencyGraphs.findModsAndFilter(graph, objectWord, obj)
   }
 
+  /**
+    * Finds words which are linked to an object word
+    * that are actually part of the predicate.
+    * @param graph
+    * @param objectEdge
+    * @return
+    */
   def getPredicatePartsFromObject(graph: SemanticGraph, objectEdge: SemanticGraphEdge): List[AuxiliaryBranch] = {
     /*
      * find cases of the object which could be part of the predicate
@@ -31,6 +58,13 @@ object PathExtraction {
     BranchExtraction.findAuxiliaryCases(graph, objectEdge.getDependent)
   }
 
+  /**
+    * Finds an auxiliary branch which for a predicate.
+    * Used when a path has no object and one branch of
+    * the predicate can be used to continue the sentence.
+    * @param predicate
+    * @return
+    */
   def candidateAuxiliaryBranch(predicate: Predicate): Option[AuxiliaryBranch] = {
     for (branch <- predicate.branches) {
       val nouns = branch.words.filter(word => CoreNLPUtil.isNoun(word))
@@ -43,6 +77,7 @@ object PathExtraction {
     None
   }
 
+  @deprecated
   def createPathFromSubjectFlow(subj: List[IndexedWord], predicate: Predicate,
                                 obj: List[IndexedWord], indirectObj: List[IndexedWord],
                                 predicateBranches: List[AuxiliaryBranch]): InformationPath =
@@ -52,6 +87,14 @@ object PathExtraction {
     InformationPath(subj, extendedPredicated, obj, indirectObj)
   }
 
+
+  /**
+    * Creates a new predicate which contains the old
+    * predicate's branches and the ones to be added.
+    * @param predicate
+    * @param branchesLists
+    * @return
+    */
   def extendPredicateBranches(predicate: Predicate, branchesLists: List[AuxiliaryBranch]*): Predicate = {
     val buffer = new ListBuffer[AuxiliaryBranch]
 
@@ -61,6 +104,19 @@ object PathExtraction {
     predicate.copy(branches = buffer.toList)
   }
 
+  /**
+    * Finds a path out of subject and predicate words.
+    * It tries to find a direct object (and an
+    * indirect one if available), if none is found
+    * then it finds an alternative branch from the the
+    * predicate to fill its position. However, the
+    * result doesn't necessarily have to contain an
+    * object.
+    * @param graph
+    * @param subjectWord
+    * @param predicateWord
+    * @return
+    */
   def findSubjectPaths(graph: SemanticGraph, subjectWord: IndexedWord,
                        predicateWord: IndexedWord): List[InformationPath] =
   {
@@ -111,6 +167,16 @@ object PathExtraction {
     paths.toList
   }
 
+  /**
+    * Finds a path out of an object word and a
+    * predicate word. It tries to find a subject
+    * but if none is found then the subject is
+    * assumed to be empty.
+    * @param graph
+    * @param objectWord
+    * @param predicateWord
+    * @return
+    */
   def findObjectPaths(graph: SemanticGraph, objectWord: IndexedWord,
                       predicateWord: IndexedWord): List[InformationPath] =
   {
@@ -136,10 +202,25 @@ object PathExtraction {
     buffer.toList
   }
 
+  /**
+    * Does the same job as findSubjectPaths but
+    * works on the case that a path starts from
+    * an appositional or a clausal modifier.
+    * @param graph
+    * @param appsAclEdge
+    * @return
+    */
   def findPathsFromApposAclEdges(graph: SemanticGraph, appsAclEdge: SemanticGraphEdge): List[InformationPath] = {
     findSubjectPaths(graph, appsAclEdge.getGovernor, appsAclEdge.getDependent)
   }
 
+  /**
+    * Finds paths from an open clausal complement
+    * case.
+    * @param graph
+    * @param xcompEdge
+    * @return
+    */
   def findPathsFromXcompEdge(graph: SemanticGraph, xcompEdge: SemanticGraphEdge): List[InformationPath] = {
     if (xcompEdge.getGovernor.tag().contains("VB") && !xcompEdge.getDependent.tag().contains("VB")) {
       val predicate = predicateFromWord(graph, xcompEdge.getGovernor)
