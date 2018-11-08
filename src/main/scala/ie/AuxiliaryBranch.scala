9836f41df98aa50ba441edf18ce31ec75b07d9d1
KLD <kld_at7@hotmail.com>
2018-11-08 10:41:32 +0300

    added javadoc to some of the classes

--- src/main/scala/ie/AuxiliaryBranch.scala
+++ src/main/scala/ie/AuxiliaryBranch.scala
@@ -3,6 +3,14 @@ package ie
 import edu.stanford.nlp.ling.IndexedWord
 import util.CoreNLPUtil
 
+/**
+  * A class to represent an branch of a word. A branch
+  * provides complementary information about the word
+  * such as its negation, or nominal dependent.
+  * @param branchType The type of the branch with respect
+  *                   to the original word.
+  * @param words The words which make up the branch.
+  */
 case class AuxiliaryBranch(branchType: String, words: List[IndexedWord]) {
   override def toString: String = branchType + ": " + CoreNLPUtil.indexWordsToString(words)
 }
